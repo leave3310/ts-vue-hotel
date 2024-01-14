8454c7ac4cb0eedabe589e42089d8fea001f1ac3
leave3310 <sampisam3@gmail.com>
2024-01-14 21:23:44 +0800

    feat: 新增veevalidate

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,33 @@
-import './assets/main.css'
+import '@/assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import { defineRule, configure } from 'vee-validate';
+import * as AllRules from '@vee-validate/rules';
+import { localize } from '@vee-validate/i18n';
+import zhTW from "@vee-validate/i18n/dist/locale/zh_TW.json";
+
+Object.keys(AllRules).forEach(rule => {
+
+    defineRule(rule, AllRules[rule]);
+});
+
+configure({
+    // Generates an English message locale generator
+    generateMessage: localize({
+        zh_TW: {
+            ...zhTW,
+            messages: {
+                ...zhTW.messages,
+                required: "此欄位為必填",
+                max: "字數上限為 0:{length} 字",
+                phone: "手機號碼只能輸入十碼",
+                email: "請輸入有效的 Email",
+            },
+        },
+    }),
+});
 
 import App from './App.vue'
 import router from './router'
